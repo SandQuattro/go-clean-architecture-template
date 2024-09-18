5bb6e1600f374a80760513948934f039533ab0bc
Igor <SandQuattro@gmail.com>
2024-09-18 19:59:26 +0300

    chore: changed logger message field, dc bug

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -20,6 +20,14 @@ func SetupLogger(cfg *config.Config) {
 		}
 		logger = slog.New(slog.NewTextHandler(os.Stdout, opts))
 	} else if os.Getenv("ENV_NAME") == "prod" {
+		key := func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.MessageKey {
+				a.Key = "message"
+				return a
+			}
+			return a
+		}
+		opts = &slog.HandlerOptions{ReplaceAttr: key}
 		logger = slog.New(slog.NewJSONHandler(os.Stdout, opts))
 	}
 
