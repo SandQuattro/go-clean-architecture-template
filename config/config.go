dd6f4d5c32b0e207ad14bf294eddfa8a54f6006c
Igor <SandQuattro@gmail.com>
2024-11-07 22:31:00 +0300

    chore: fix

--- config/config.go
+++ config/config.go
@@ -3,45 +3,48 @@ package config
 import (
 	"fmt"
 	"log/slog"
+	"net"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type (
 	Config struct {
-		App     `json:"app"`
-		HTTP    `json:"http"`
-		DB      `json:"db"`
-		Log     `json:"logger"`
-		Tracing `json:"tracing"`
+		App     `json:"app"     toml:"app"`
+		HTTP    `json:"http"    toml:"http"`
+		DB      `json:"db"      toml:"db"`
+		Log     `json:"logger"  toml:"logger"`
+		Tracing `json:"tracing" toml:"tracing"`
 	}
 
 	App struct {
-		Name    string `env-required:"false" json:"name"    env:"APP_NAME"`
-		Version string `env-required:"false" json:"version" env:"APP_VERSION"`
+		Name    string `json:"name"    toml:"name"    env:"APP_NAME"`
+		Version string `json:"version" toml:"version" env:"APP_VERSION"`
 	}
 
 	HTTP struct {
-		Port string `env-required:"false" json:"port" env:"HTTP_PORT"`
+		Port string ` json:"port" toml:"port" env:"HTTP_PORT"`
 	}
 
 	DB struct {
-		DBHost     string `env-required:"false" json:"host"     env:"DB_HOST"`
-		DBPort     int    `env-required:"false" json:"port"     env:"DB_PORT"`
-		DBUser     string `env-required:"false" json:"user"     env:"DB_USER"`
-		DBPassword string `env-required:"true"  json:"password" env:"DB_PASSWORD"`
-		DBName     string `env-required:"false" json:"name"     env:"DB_NAME"`
-		PoolMax    int32  `env-required:"true"  json:"pool_max" env:"PG_POOL_MAX"`
+		DBHost     string `json:"host"     toml:"host"     env:"DB_HOST"`
+		DBPort     int    `json:"port"     toml:"port"     env:"DB_PORT"`
+		DBUser     string `json:"user"     toml:"user"     env:"DB_USER"`
+		DBPassword string `json:"password" toml:"password" env:"DB_PASSWORD" env-required:"true"`
+		DBName     string `json:"name"     toml:"name"     env:"DB_NAME"`
+		PoolMax    int32  `json:"pool_max" toml:"pool_max" env:"PG_POOL_MAX" env-required:"true"`
 	}
 
 	Log struct {
-		Level slog.Level `env-required:"false" json:"level"   env:"LOG_LEVEL"`
+		Level slog.Level `json:"level" toml:"level" env:"LOG_LEVEL"`
 	}
 
 	Tracing struct {
-		URL string `env-required:"false" json:"url" env:"TRACING_URL"`
+		URL string ` json:"url" env:"TRACING_URL"`
 	}
 )
 
@@ -61,6 +64,18 @@ func LoadConfig() (*Config, error) {
 	err = cleanenv.ReadEnv(cfg)
 	if err != nil {
 		return nil, fmt.Errorf("env read error: %w", err)
+	}
+
+	if strings.Contains(cfg.DBHost, ":") {
+		host, port, e := net.SplitHostPort(cfg.DBHost)
+		if e != nil {
+			return nil, e
+		}
+		cfg.DBHost = host
+		cfg.DBPort, err = strconv.Atoi(port)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return cfg, nil
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v5 v5.7.0
 	github.com/pashagolub/pgxmock/v4 v4.3.0
+	github.com/prometheus/client_golang v1.19.0
 	github.com/stretchr/testify v1.9.0
 	go.opentelemetry.io/otel v1.29.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.29.0
@@ -55,7 +56,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.19.0 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.53.0 // indirect
 	github.com/prometheus/procfs v0.14.0 // indirect
